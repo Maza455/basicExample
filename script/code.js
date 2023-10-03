41afb09c6731a20c92abb89d2513b8719e4c9956
Maza455 <same7nte@gmail.com>
2023-10-03 15:44:43 +0200

    I've made changes in Code.js

--- script/code.js
+++ script/code.js
@@ -7,4 +7,4 @@
 // let myButton = document.getElementById("zuks");
 
 console.log('My name is Zukisa Maza');
-console.log('I am willing to learn coding');
+console.log('I am willing to learn coding and run my own cyber security company');
