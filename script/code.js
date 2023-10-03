d967b603fe7e60855a31e28d5b923a3a98b7db91
Maza455 <same7nte@gmail.com>
2023-10-03 14:36:11 +0200

    Adding first commit

---
+++ script/code.js
@@ -0,0 +1,10 @@
+// myFunction(); {
+//     console.log('uyaphapha maarn')
+// }
+
+// myFunction();
+
+// let myButton = document.getElementById("zuks");
+
+console.log('My name is Zukisa Maza');
+console.log('I am willing to learn coding');
